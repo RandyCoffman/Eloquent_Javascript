6aaab122468be9594367fa1d932d055df220c22a
RandyCoffman <randy.coffman@wvjc.edu>
2018-10-22 08:47:02 -0400

    first commit

---
+++ chapter2_exercises.js
@@ -0,0 +1,44 @@
+function Triangle(num) {
+    var printingVar = "#"
+    while(printingVar.length <=num ){
+        console.log(printingVar);
+        printingVar = printingVar + "#";
+    }
+}
+
+
+function FizzBuzz(){
+    for (var i = 0; i <= 100; i++) {
+        switch (true) {
+            case (i % 5 === 0 && i % 3 === 0):
+                console.log("FizzBuzz");
+                break;
+            case i % 3 === 0:
+                console.log("Fizz");
+                break;
+            case i % 5 === 0:
+                console.log("Buzz");
+                break;
+            default:
+                console.log(i);
+                break;
+        }
+    }
+}
+
+function Chessboard(boardSize){
+    var size = boardSize;
+    var character = "";
+    
+    for (var r = 0; r < size; r++) { // row
+        for (var c = 0; c < size; c++) { // column
+            if ((r + c) % 2 == 0){
+                character += " ";
+            }else{
+                character += "#";
+            }
+        }
+    character += "\n";
+    }
+    console.log(character);
+}
