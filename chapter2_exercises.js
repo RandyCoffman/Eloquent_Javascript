cdd0cbb90a770162f48b38155d80ae87ac8a5a0e
Scott Coffman <sc.coffman45@ymail.com>
2018-10-31 14:10:22 -0400

    Changed position of a case statement, put i % 5 over top of i % 3

--- chapter2_exercises.js
+++ chapter2_exercises.js
@@ -13,11 +13,11 @@ function FizzBuzz(){
             case (i % 5 === 0 && i % 3 === 0):
                 console.log("FizzBuzz");
                 break;
-            case i % 3 === 0:
-                console.log("Fizz");
-                break;
             case i % 5 === 0:
                 console.log("Buzz");
+                break;
+            case i % 3 === 0:
+                console.log("Fizz");
                 break;
             default:
                 console.log(i);
